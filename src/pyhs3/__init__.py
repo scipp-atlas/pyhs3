1c4a52ee995f32d5c7bad8044d5d66cfca0ac6dc
Giordon Stark <kratsg@gmail.com>
2025-01-22 18:26:37 +0100

    feat: Add simplemodels and tests for simplemodels (#6)
    * add uncorrelated background from pyhf's [HelloWorld](https://pyhf.github.io/pyhf-tutorial/HelloWorld.html)
    * add test that will be expected to fail until the structure is fleshed out more

--- src/pyhs3/__init__.py
+++ src/pyhs3/__init__.py
@@ -6,6 +6,34 @@ pyhs3: pure-Python HS3 implementation with tensors and autodiff
 
 from __future__ import annotations
 
+from typing import Any
+
 from ._version import version as __version__
 
-__all__ = ["__version__"]
+
+class Workspace:
+    """
+    HS3 Workspace Object
+    """
+
+    def __init__(self, _spec: dict[str, Any]): ...
+    def model(self) -> Model:
+        return Model()
+
+    def data(self) -> list[float]:
+        return [0.0, 0.0, 0.0, 0.0]
+
+
+class Model:
+    """
+    HS3 Model Object
+    """
+
+    def pdf(self, _pars: list[float], _data: list[float]) -> float:
+        return 0.0
+
+    def logpdf(self, _pars: list[float], _data: list[float]) -> float:
+        return 0.0
+
+
+__all__ = ["__version__", "Workspace"]
---
+++ tests/test_hifa.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import json
+
+import pyhf
+import pytest
+
+import pyhs3
+
+
+@pytest.mark.xfail(reason="To be implemented")
+@pytest.mark.parametrize(
+    ("pars"),
+    [[i, j, k] for i in [0.0, 1.0] for j in [0.0, 1.0] for k in [0.0, 1.0]],
+)
+def test_simplemodel_pyhf(pars, datadir):
+    """
+    To convert the pyhf simplemodel in HiFa JSON to HS3 JSON:
+
+        $ pyhf xml2json <hifa.json> --output-dir hs3
+        $ cd hs3
+        $ hist2workspace FitConfig.xml
+        $ root -b config/FitConfig_combined_measurement_model.root
+        root [1] combined
+        (RooWorkspace *) 0x1362aac00
+        root [2] auto mytool = RooJSONFactoryWSTool(*combined);
+        root [3] mytool.exportJSON("<hs3.json>")
+        (bool) true
+    """
+    ws_pyhf = pyhf.Workspace(
+        json.loads(
+            datadir.joinpath(
+                "simplemodel_uncorrelated-background_hifa.json"
+            ).read_text()
+        )
+    )
+    model_pyhf = ws_pyhf.model()
+    data_pyhf = ws_pyhf.data(model_pyhf)
+
+    ws_pyhs3 = pyhs3.Workspace(
+        json.loads(
+            datadir.joinpath("simplemodel_uncorrelated-background_hs3.json").read_text()
+        )
+    )
+    model_pyhs3 = ws_pyhs3.model()
+    data_pyhs3 = ws_pyhs3.data()
+
+    assert model_pyhs3.pdf(pars, data_pyhs3) == pytest.approx(
+        model_pyhf.pdf(pars, data_pyhf)
+    )
+    assert model_pyhs3.logpdf(pars, data_pyhs3) == pytest.approx(
+        model_pyhf.logpdf(pars, data_pyhf)
+    )

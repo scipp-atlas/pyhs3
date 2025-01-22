67ba455b72b9d893d1a2475f42ce22ff39605446
Giordon Stark <kratsg@gmail.com>
2025-01-21 20:23:16 -0800

    add hs3 model in

--- tests/test_hifa.py
+++ tests/test_hifa.py
@@ -13,6 +13,19 @@ import pyhs3
     [[i, j, k] for i in [0.0, 1.0] for j in [0.0, 1.0] for k in [0.0, 1.0]],
 )
 def test_simplemodel_pyhf(pars, datadir):
+    """
+    To convert the pyhf simplemodel in HiFa JSON to HS3 JSON:
+
+        $ pyhf xml2json <hifa.json> --output-dir hs3
+        $ cd hs3
+        $ hist2workspace FitConfig.xml
+        $ root -b config/FitConfig_combined_measurement_model.root
+        root [1] combined
+        (RooWorkspace *) 0x1362aac00
+        root [2] auto mytool = RooJSONFactoryWSTool(*combined);
+        root [3] mytool.exportJSON("<hs3.json>")
+        (bool) true
+    """
     ws_pyhf = pyhf.Workspace(
         json.loads(
             datadir.joinpath(
